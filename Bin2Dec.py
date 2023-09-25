e0d9ea1d457f43d28d2b1850dc7f7acbb51ce606
Felipe <felipe.obsousa@gmail.com>
2023-09-25 18:40:30 -0300

    criando conversor de numero binario
    criando e aprendendo sobre numeros binarios e como converte-los

---
+++ Bin2Dec.py
@@ -0,0 +1,10 @@
+## User Stories
+#tabela de numeros binarios
+'''https://www.ime.usp.br/~pf/algoritmos/apend/ascii.html'''
+
+#    [ ] User can enter up to 8 binary digits in one input field]
+nb = int(input("digite seu codigo binario"))
+if nb 
+
+#-   [ ] User must be notified if anything other than a 0 or 1 was entered
+#-   [ ] User views the results in a single output field containing the decimal (base 10) equivalent of the binary number that was entered
