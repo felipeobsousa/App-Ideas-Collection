fade28377d8f6c5e8bc432631a9f724e4b2d313b
Felipe <felipe.obsousa@gmail.com>
2023-10-25 10:40:29 -0300

    Update Bin2Dec.py

--- Bin2Dec.py
+++ Bin2Dec.py
@@ -1,10 +1,16 @@
 ## User Stories
-#tabela de numeros binarios
-'''https://www.ime.usp.br/~pf/algoritmos/apend/ascii.html'''
 
 #    [ ] User can enter up to 8 binary digits in one input field]
-nb = int(input("digite seu codigo binario"))
-if nb 
+
+numero_binario = str("00110001 00111001 00111001 00110111 00001010")
+quantidade_de_caracters = (len(numero_binario))
+if quantidade_de_caracters < 8:
+    print("seu numero deve conter mais do que oito caracteres")
 
 #-   [ ] User must be notified if anything other than a 0 or 1 was entered
+
+
+
 #-   [ ] User views the results in a single output field containing the decimal (base 10) equivalent of the binary number that was entered
+
+
